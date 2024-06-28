48f81f1f2d1e5d40390359d5e1436d8496cb97d4
admin44449999 <108731844+admin44449999@users.noreply.github.com>
2024-06-28 21:16:56 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -9,7 +9,7 @@ if ENVIRONMENT:
         raise Exception("Your API_ID is not a valid integer.")
     API_HASH = os.environ.get('API_HASH', "b3611c291bf82d917637d61e4a136535")
     BOT_TOKEN = os.environ.get('BOT_TOKEN', "6616252751:AAEHX0kpIqUr-vvqDWzR-oq0tdgwzQC4Y0s")
-    DATABASE_URL = os.environ.get('DATABASE_URL', "")
+    DATABASE_URL = os.environ.get('DATABASE_URL', "postgresql://postgres.adldmnbzdckqwrckabtj:Aaryanary1097@aws-0-ap-south-1.pooler.supabase.com:6543/postgres")
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
     # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
     MUST_JOIN = os.environ.get('MUST_JOIN', "leechbot449")
